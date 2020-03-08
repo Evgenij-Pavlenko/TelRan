fc9a118c5a9d1997535e0cd22e8b126aafdc01f4
Evgenij <Evgenij@10.0.2.15>
2020-03-08 20:20:29 +0100

    search max Value

--- src/com/telran/array/Arrays.java
+++ src/com/telran/array/Arrays.java
@@ -10,12 +10,14 @@ public class Arrays {
 		int[] arrayInt = new int[aLeyght];
 		
 		arrayAdd(arrayInt);
+		arrayAddMega(arrayInt); // all value Int
 		arrayPrint(arrayInt);
-//		arrayMax(arrayInt);
+		System.out.println("Max value in Array is: " + arrayMax(arrayInt));
 		
 		
 
 	}
+	
 	/*
 	 * add elements in Array
 	 */
@@ -27,12 +29,23 @@ public class Arrays {
 		}
 		
 	}
+	/*
+	 * add elements in Array all int
+	 */
+	public static void arrayAddMega(int [] arrayInt) {
+		
+		for (int i = 0; i < arrayInt.length; i++) {
+			arrayInt[i] = (int) (Math.random()*(Integer.MAX_VALUE) - Integer.MAX_VALUE);
+			
+		}
+		
+	}
 	
 	/*
-	 * print Array as
+	 * print Array as [1, 2, 3]
 	 */
 	private static void arrayPrint(int[] arrayInt) {
-		System.out.println("Array with leght " + arrayInt.length);
+		System.out.println("Array with leght " + arrayInt.length + ":");
 		System.out.print("[");
 		for (int i = 0; i < arrayInt.length; i++) {
 			if (i != arrayInt.length-1) {
@@ -42,7 +55,21 @@ public class Arrays {
 			
 		}
 		System.out.print("]");
+		System.out.println("");
 		
+	}
+	/*
+	 * find Max element in Array
+	 */
+	private static int arrayMax(int[] arrayInt) {
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < arrayInt.length; i++) {
+			if (arrayInt[i] > max) {
+				max = arrayInt[i];
+				
+			}
+		}
+		return max;
 	}
 	
 	
