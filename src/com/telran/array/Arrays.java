775843f12815db8721229e65b1b72b3d24f91e3e
Evgenij <Evgenij@10.0.2.15>
2020-03-08 20:00:49 +0100

    print Array

--- src/com/telran/array/Arrays.java
+++ src/com/telran/array/Arrays.java
@@ -10,16 +10,38 @@ public class Arrays {
 		int[] arrayInt = new int[aLeyght];
 		
 		arrayAdd(arrayInt);
-		System.out.println(arrayInt);
+		arrayPrint(arrayInt);
+//		arrayMax(arrayInt);
+		
 		
 
 	}
-	public static void arrayAdd(int [] arr) {
+	/*
+	 * add elements in Array
+	 */
+	public static void arrayAdd(int [] arrayInt) {
 		
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = (int) (Math.random()*rand);
+		for (int i = 0; i < arrayInt.length; i++) {
+			arrayInt[i] = (int) (Math.random()*rand);
 			
 		}
+		
+	}
+	
+	/*
+	 * print Array as
+	 */
+	private static void arrayPrint(int[] arrayInt) {
+		System.out.println("Array with leght " + arrayInt.length);
+		System.out.print("[");
+		for (int i = 0; i < arrayInt.length; i++) {
+			if (i != arrayInt.length-1) {
+				System.out.print(arrayInt[i] + ", ");
+			}
+			else System.out.print(arrayInt[i]);
+			
+		}
+		System.out.print("]");
 		
 	}
 	
